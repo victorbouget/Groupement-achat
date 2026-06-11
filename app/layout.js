7161a09e9289217ec0f046c7af8b8c25e643d9e9
VBouget <victor.bouget@gmail.com>
2026-06-11 22:39:56 +0200

    fix viewport mobile v2

--- app/layout.js
+++ app/layout.js
@@ -35,6 +35,9 @@ export default function RootLayout({ children }) {
       lang="fr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body
         className="min-h-full flex flex-col"
         style={{
